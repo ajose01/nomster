de7c6a002600c249868acc9f7b84cdb9ccfb22b0
Angel Jose <ajose01@gmail.com>
2014-09-10 21:52:29 +0000

    Added a new form to input items into database.  Installed simple_form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,5 +3,10 @@ class PlacesController < ApplicationController
 		@places = Place
 		.all
 		.paginate(:page => params[:page], :per_page => 2)
-			end
+	end
+
+	def new
+		@place = Place.new
+	end
+
 end
